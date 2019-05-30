c2fbf140401e77f9171203c92a1348de0de86c62
okit39 <45716257+okit39@users.noreply.github.com>
2019-05-30 07:15:08 +0200

    Add files via upload

---
+++ AddingClothesActivity.java
@@ -0,0 +1,203 @@
+package com.example.user.wardrobe2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class AddingClothesActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_PERMISSION_RECEIVE_CAMERA = 102;
+    private static final int REQUEST_CODE_TAKE_PHOTO = 103;
+
+    private File mTempPhoto;
+    private String mImageUri = "";
+    private String mRereference = "";
+
+    private FirebaseAuth mAuth;
+    private StorageReference mStogageReference;
+    private StorageReference mStorageRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adding_clothes);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Spinner category = findViewById(R.id.spinner_category);
+        Spinner color = findViewById(R.id.spinner_color);
+        final ImageView photo = findViewById(R.id.photo);
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        File localFile = null;
+        mRereference = getIntent().getStringExtra("Reference");
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        try {
+            localFile = createTempImageFile(getExternalCacheDir());
+            final File finalLocalFile = localFile;
+
+            mStorageRef.child("images/" + mRereference).getFile(localFile)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            Picasso.with(getBaseContext())
+                                    .load(Uri.fromFile(finalLocalFile))
+                                    .into(photo);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.i("Load","" + e);
+                }
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addPhoto();
+                //отобразить фото в imageview
+            }
+        });
+
+        ImageButton yes = findViewById(R.id.btn_yes);
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //сохранить все данные (фото, категория и цвет)
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton no = findViewById(R.id.btn_no);
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //отмена всех действий
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    //Метод для добавления фото
+    private void addPhoto() {
+
+        //Проверяем разрешение на работу с камерой
+        boolean isCameraPermissionGranted = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+        //Проверяем разрешение на работу с внешнем хранилещем телефона
+        boolean isWritePermissionGranted = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+
+        //Если разрешения != true
+        if(!isCameraPermissionGranted || !isWritePermissionGranted) {
+
+            String[] permissions;//Разрешения которые хотим запросить у пользователя
+
+            if (!isCameraPermissionGranted && !isWritePermissionGranted) {
+                permissions = new String[] {android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            } else if (!isCameraPermissionGranted) {
+                permissions = new String[] {android.Manifest.permission.CAMERA};
+            } else {
+                permissions = new String[] {android.Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            }
+            //Запрашиваем разрешения у пользователя
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE_PERMISSION_RECEIVE_CAMERA);
+        } else {
+            //Если все разрешения получены
+            try {
+                mTempPhoto = createTempImageFile(getExternalCacheDir());
+                mImageUri = mTempPhoto.getAbsolutePath();
+
+                //Создаём лист с интентами для работы с изображениями
+                List<Intent> intentList = new ArrayList<>();
+                Intent chooserIntent = null;
+
+
+                Intent pickIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                takePhotoIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mTempPhoto));
+
+                intentList = addIntentsToList(this, intentList, pickIntent);
+                intentList = addIntentsToList(this, intentList, takePhotoIntent);
+
+                if (!intentList.isEmpty()) {
+                    chooserIntent = Intent.createChooser(intentList.remove(intentList.size() - 1),"Choose your image source");
+                    chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentList.toArray(new Parcelable[]{}));
+                }
+
+                /*После того как пользователь закончит работу с приложеним(которое работает с изображениями)
+                 будет вызван метод onActivityResult
+                */
+                startActivityForResult(chooserIntent, REQUEST_CODE_TAKE_PHOTO);
+            } catch (IOException e) {
+                Log.e("ERROR", e.getMessage(), e);
+            }
+        }
+    }
+
+    public static File createTempImageFile(File storageDir) throws IOException {
+
+        // Генерируем имя файла
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());//получаем время
+        String imageFileName = "photo_" + timeStamp;//состовляем имя файла
+
+        //Создаём файл
+        return File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+    }
+
+    //Метод для добавления интента в лист интентов
+
+    public static List<Intent> addIntentsToList(Context context, List<Intent> list, Intent intent) {
+        List<ResolveInfo> resInfo = context.getPackageManager().queryIntentActivities(intent, 0);
+        for (ResolveInfo resolveInfo : resInfo) {
+            String packageName = resolveInfo.activityInfo.packageName;
+            Intent targetedIntent = new Intent(intent);
+            targetedIntent.setPackage(packageName);
+            list.add(targetedIntent);
+        }
+        return list;
+    }
+
+}
